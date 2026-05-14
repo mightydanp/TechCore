f42540f63f2edd6916a96786885b4971b9e9cc42
mightydanp <chrisdunbar12@gmail.com>
2026-05-14 05:05:18 -0500

    -added small, tiny, div72 textures for crushed_ore, purified_ore, centrifuged_ore -grayscaled crushed_ore_overlay.png and crushed_tiny_ore_overlay.png -removed magnetic from material_icons because I added it the 2 different red and blue magnetic parts that face left and right in the overlay to overlay_icons to be used as an overlay in PhysicalComponent

---
+++ src/main/java/com/mightydanp/techcore/materials/Item/OreItem.java
@@ -0,0 +1,25 @@
+package com.mightydanp.techcore.materials.Item;
+
+import com.mightydanp.techcore.materials.properties.MaterialProperties;
+import net.minecraft.world.item.ItemStack;
+
+public class OreItem extends MaterialItem{
+    public OreItem(MaterialProperties properties) {
+        super(properties.stacksTo(1));
+    }
+
+    public float getQuantityLevel(ItemStack itemStack) {
+        Integer quantity = ((OreItem)itemStack.getItem()).getQuantity(itemStack);
+        Integer maxQuantity = ((OreItem)itemStack.getItem()).getMaxQuantity();
+
+        if(maxQuantity == null || quantity == null) return -1;
+
+        if(quantity <= (maxQuantity / 72)) return 0.0f;//div72
+
+        if(quantity <= (maxQuantity / 9)) return 0.25f;//tiny
+
+        if(quantity <= (maxQuantity / 4)) return 0.50f;//small
+
+        return 1.0f;//full
+    }
+}
